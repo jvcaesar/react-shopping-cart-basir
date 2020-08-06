23773f4f50a4d218582ea9c481d4b6dd7fc967d1
John Caesar <john.v.caesar@gmail.com>
2020-08-06 20:06:42 +0300

    add redux to filter component

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { FETCH_PRODUCTS } from "../types"
+import { FETCH_PRODUCTS, FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types"
 
 export const fetchProducts = () => async (dispatch) => {
     const result = await fetch('/api/products')
@@ -9,3 +9,38 @@ export const fetchProducts = () => async (dispatch) => {
         payload: data
     })
 }
+
+const sortProduct = (sort, prodList) => {
+    const items = prodList.slice().sort((a, b) => (
+    sort === 'lowest' ? ((a.price > b.price) ? 1 : -1) :
+    sort === 'highest' ? ((a.price < b.price) ? 1 : -1) :
+    ((a._id > b._id) ? 1 : -1)
+    ))
+    console.log('Sorted items: ', items)
+    return items
+}
+
+export const filterProducts = (products, size, sort) => (dispatch) => {
+    console.log('Action Filter:', products, size)
+    const items = size === 'ALL' ? products : 
+            products.filter(item => item.availableSizes.indexOf(size) >= 0)
+    dispatch({
+        type: FILTER_PRODUCTS_BY_SIZE,
+        payload: {
+            size: size,
+            items: sortProduct(sort, items)
+        }
+    })
+}
+
+export const sortProducts = (prodList, sort) => (dispatch) => {
+    console.log('Action Sort:', prodList, sort)
+    dispatch({
+        type: ORDER_PRODUCTS_BY_PRICE,
+        payload: {
+            sort: sort,
+            items: sortProduct(sort, prodList)
+        }
+    })
+}
+
